9481781052d550ac44714d783ec8ee21bf0e43d3
Linnéa <linnea.ternevik@gmail.com>
2025-10-07 18:14:39 +0200

    Add: ConsoleColors utility with ANSI color codes

--- src/util/ConsoleColors.java
+++ src/util/ConsoleColors.java
@@ -1,4 +1,12 @@
 package util;
 
 public class ConsoleColors {
+    private ConsoleColors() {}
+
+    public static final String PURPLE = "\u001B[35m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String RESET = "\u001B[0m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String GRAY = "\u001B[90m";
+    public static final String RED = "\u001B[31m";
 }
